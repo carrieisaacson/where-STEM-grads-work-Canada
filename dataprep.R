ccb48826bc7b5fc2edcb8729b514098bb6cc5a29
carriesmith <carrie.elizabeth@gmail.com>
2015-04-29 13:58:09 -0400

    Data table organized to mutually exclusive NOC grouping labelled STEM or non-STEM

--- dataprep.R
+++ dataprep.R
@@ -1,6 +1,6 @@
 ## Libraries ####
 library(magrittr)
-
+library(dplyr)
 ## Read Data ####
 
 # Read first two rows - combine into single header
@@ -35,17 +35,57 @@ dat$Occupation <- gsub("\\s{2}", "", dat$Occupation)
 sub(" ", "@", dat$Occupation) %>% strsplit(., "@") %>% do.call(rbind,.) %>% .[,1] -> dat$NOC
 sub(" ", "@", dat$Occupation) %>% strsplit(., "@") %>% do.call(rbind,.) %>% .[,2] -> dat$NOCdesc
 
-head(dat)
+# These NOC codes are more or less complete categories "non-STEM"
+NOC_nonSTEM <- c("A", 
+              "B", 
+              "D",
+              "E01", "E1", "E2",
+              "F", 
+              "G", 
+              "H", 
+              "I", 
+              "J")
+dat_nonSTEM <- subset(dat, dat$NOC %in% NOC_nonSTEM)
 
-dat$NOC[grep("C", dat$NOC)]
+# Category E02 - E021 (Psychologists) = 
+#       E022:E025 Social Workers, Counsellors, Clergy and Probation Officers == non-STEM NOC
+dat_nonSTEM_E02 <- subset(dat, dat$NOC %in% paste("E02", 2:5, sep=""))
+dat_nonSTEM_E02 <- data.frame( Occupation = "E02 Social workers, counsellors, clergy and probation officers (less Psychologists)",
+                               t(dat_nonSTEM_E02 %>% select(Education_STEM:Other_fields_of_study_nonSTEM) %>% colSums),
+                               NOC = "E02 (omit E021)", 
+                               NOCdesc = "Social workers, counsellors, clergy and probation officers")
 
-sum(dat[dat$NOC=="C1",][2:25])
+# Category E03 - Policy and Program Officers, Researchers and Consultants
+# E033, E035, E036, E037 and E039 are not STEM NOC
+dat_nonSTEM_E03 <- subset(dat, dat$NOC %in% paste("E03", c(3, 5, 6, 7, 9), sep=""))
+dat_nonSTEM_E03 <- data.frame( Occupation = "E03 non-STEM Policy and program officers, researchers and consultants",
+                               t(dat_nonSTEM_E03 %>% select(Education_STEM:Other_fields_of_study_nonSTEM) %>% colSums),
+                               NOC = "E033/5/6/7/9", 
+                               NOCdesc = "Policy and program officers, researchers and consultants (non-STEM)")
 
-NOCkeep <- c("A", "B", paste("C0", 1:7, sep=""), "C1", "D", "F", "G", "H", "I", "J", )
+# Combine all non-STEM NOC rows
+dat_nonSTEM <- merge(dat_nonSTEM, dat_nonSTEM_E02, all.x = T, all.y = T)
+dat_nonSTEM <- merge(dat_nonSTEM, dat_nonSTEM_E03, all.x = T, all.y = T)
+dat_nonSTEM$stem <- F
 
-# These categories are fine as simply non-STEM: "E01", "E1", "E2"
-# but E02 and EO3 NOC codes do not subdivide neatly into STEM / non-STEM
-# Going to have to do a little figuring out
-NOC_E
-          
-             
+# These NOC codes are more or less complete categories "STEM"
+# NOC C and E021 Pyschologists == STEM NOC
+NOC_STEM <- c(paste("C0", 1:7, sep=""), "C1", "E021")
+dat_STEM <- subset(dat, dat$NOC %in% NOC_STEM)
+
+# Category E03 - Policy and Program Officers, Researchers and Consultants
+# E031, E032, E034, and E038 are STEM NOC
+
+dat_STEM_E03 <- subset(dat, dat$NOC %in% paste("E03", c(1, 2, 4, 8), sep=""))
+dat_STEM_E03 <- data.frame( Occupation = "E03 STEM Policy and program officers, researchers and consultants",
+                               t(dat_STEM_E03 %>% select(Education_STEM:Other_fields_of_study_nonSTEM) %>% colSums),
+                               NOC = "E031/2/4/8", 
+                               NOCdesc = "Policy and program officers, researchers and consultants (STEM)")
+
+# Combine all STEM NOC rows
+dat_STEM <- merge(dat_STEM, dat_STEM_E03, all.x = T, all.y = T)
+dat_STEM$stem <- T
+
+dat <- merge(dat_nonSTEM, dat_STEM, all.x = T, all.y = T)
+
+write.csv(dat, "../Table - Major Field STEM vs NOC STEM.csv")
